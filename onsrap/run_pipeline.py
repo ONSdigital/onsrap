d15516bbb87dba8d421114f23777db88ab1f0e92
Sophie Pike <sophie.pike@ons.gov.uk>
2026-07-01 16:39:24 +0100

    Completed docstrings for pipeline.py, runner.py, run_pipeline.py, logger.py, and models.py. Few points remain to pick up later

--- onsrap/run_pipeline.py
+++ onsrap/run_pipeline.py
@@ -2,6 +2,10 @@ from __future__ import annotations
 
 from .runner import main
 
-
+"""
+This line of code establishes the function that must be called 
+for the Pipeline to run. main() is parsed to SystemExit as once 
+the main() function is run, this will then exit the system.
+"""
 if __name__ == "__main__":
     raise SystemExit(main())
